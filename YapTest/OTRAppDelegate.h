eaed018f85beccb4793125e218bbeaae5b48447f
Chris Ballinger <chrisballinger@gmail.com>
2014-03-20 21:49:39 -0700

    Initial Commit

---
+++ YapTest/OTRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OTRAppDelegate.h
+//  YapTest
+//
+//  Created by Christopher Ballinger on 3/20/14.
+//  Copyright (c) 2014 ChatSecure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YapTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YapTest
+//
+//  Created by Christopher Ballinger on 3/20/14.
+//  Copyright (c) 2014 ChatSecure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OTRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OTRAppDelegate class]));
+    }
+}
